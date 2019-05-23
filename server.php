34e7b1704f059eef48e1985494d989e1cbb1ae97
Mior Muhammad Zaki <crynobone@gmail.com>
2019-05-23 10:29:04 +0800

    Tweak message.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- server.php
+++ server.php
@@ -8,13 +8,18 @@ $server = new React\Socket\TcpServer(50001, $eventLoop);
 
 $server->on('connection', function (React\Socket\ConnectionInterface $connection) use ($writer) {
     $writer->write('Connection from '.$connection->getRemoteAddress().PHP_EOL);
+    $address = $connection->getRemoteAddress();
 
-    $connection->on('data', function ($chunk) use ($connection, $writer) {
-        $writer->write($connection->getRemoteAddress()." send :`{$chunk}`".PHP_EOL);
+    $connection->on('data', function ($chunk) use ($address, $writer) {
+        $writer->write("{$address} send :`{$chunk}`".PHP_EOL);
     });
 
-    $connection->on('close', function ($chunk) use ($connection, $writer) {
-        $writer->write($connection->getRemoteAddress().' closed connection'.PHP_EOL);
+    $connection->on('end', function () use ($address, $writer) {
+        $writer->write("{$address} connection ended".PHP_EOL);
+    });
+
+    $connection->on('close', function () use ($address, $writer) {
+        $writer->write("{$address} connection closed".PHP_EOL);
     });
 });
 
